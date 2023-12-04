6c95004e3522bbc62d4ccc37bec7b5b1ce380a29
Mich3000 <User@MICH3000>
2023-12-04 12:01:53 +0100

    #modif data.h

--- ProjetPoo/data.h
+++ ProjetPoo/data.h
@@ -1,1 +1,51 @@
 #pragma once
+#include <iostream>
+
+
+using namespace std;
+
+namespace data {
+
+    struct Adresse {
+
+        int numero;
+        string ville;
+        string codePostal;
+    };
+
+    struct Client {
+        string nomCl;
+        string prenomCl;
+        string dateNa;
+        Adresse adresseFacturation;
+        Adresse adresseLivraison;
+        string societe;
+    };
+
+    struct Personnel {
+
+        string nomP;
+        string prenomP;
+        string dateEm;
+        string superviseur;
+        Adresse adresse;
+    };
+
+    struct Commande {
+
+        string refCom;
+        string dateliv;
+        int total;
+        string dateemision;
+    };
+
+    struct Article {
+
+        string nomArt;
+        int stock;
+        float seuilReap;
+        float prixHT;
+        float  seuil;
+    };
+
+}
