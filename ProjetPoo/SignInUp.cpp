183d14a68fd8319e6f2a4ff6c668094629554996
Mich3000 <User@MICH3000>
2023-12-04 16:31:10 +0100

    #creation SignInUp

---
+++ ProjetPoo/SignInUp.cpp
@@ -0,0 +1,2 @@
+﻿#include "SignInUp.h"
+
---
+++ ProjetPoo/SignInUp.h
@@ -0,0 +1,157 @@
+﻿#pragma once
+
+namespace ProjetPoo {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for SignInUp
+	/// </summary>
+	public ref class SignInUp : public System::Windows::Forms::Form
+	{
+	public:
+		SignInUp(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~SignInUp()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ Labelmail;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ LaPrenom;
+
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->Labelmail = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->LaPrenom = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// Labelmail
+			// 
+			this->Labelmail->AutoSize = true;
+			this->Labelmail->Location = System::Drawing::Point(0, 0);
+			this->Labelmail->Name = L"Labelmail";
+			this->Labelmail->Size = System::Drawing::Size(32, 13);
+			this->Labelmail->TabIndex = 0;
+			this->Labelmail->Text = L"Mail :";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 28);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(77, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Mot de passe :";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(0, 64);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(134, 13);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Confirmer le mot de passe :";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 103);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(35, 13);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"Nom :";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(12, 151);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(96, 13);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Date de naissance";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(211, 195);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(43, 13);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"Société";
+			// 
+			// LaPrenom
+			// 
+			this->LaPrenom->AutoSize = true;
+			this->LaPrenom->Location = System::Drawing::Point(73, 103);
+			this->LaPrenom->Name = L"LaPrenom";
+			this->LaPrenom->Size = System::Drawing::Size(43, 13);
+			this->LaPrenom->TabIndex = 6;
+			this->LaPrenom->Text = L"Prénom";
+			// 
+			// SignInUp
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1280, 720);
+			this->Controls->Add(this->LaPrenom);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->Labelmail);
+			this->Name = L"SignInUp";
+			this->Text = L"SignInUp";
+			this->Load += gcnew System::EventHandler(this, &SignInUp::SignInUp_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void SignInUp_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
